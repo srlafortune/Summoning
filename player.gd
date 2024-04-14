a2fb991c4b7e699e3fa2237e94450d43c8b41a9d
Stephen Lafortune <slafor24@gmail.com>
2024-04-14 13:50:19 -0400

    Animation working now

--- player.gd
+++ player.gd
@@ -1,39 +1,29 @@
 extends CharacterBody2D
 
 @export var speed = 400
-var angular_speed = 100.0
-@export var golem_scene: PackedScene
-
-var golem_count = 2;
 
 func _ready():
-	$GolemsPath.rotation_degrees = -180
-	# add_golems()
+	pass
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
+
+	if Input.is_action_pressed("right"):
+		$AnimatedSprite2D.animation = "right"
+	if Input.is_action_pressed("left"):
+		$AnimatedSprite2D.animation = "left"
+	if Input.is_action_pressed("up"):
+		$AnimatedSprite2D.animation = "up"
+	if Input.is_action_pressed("down"):
+		$AnimatedSprite2D.animation = "down"
+
+	if velocity.length() > 0:
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+
 	velocity = input_direction * speed
 
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
-
-	var current_angle = $GolemsPath.rotation
-	var delta_angle = rad_to_deg((velocity * - 1).angle() - current_angle)
-
-	if delta_angle > 180:
-			delta_angle -= 360
-	elif delta_angle < - 180:
-			delta_angle += 360
-
-	var direction = sign(delta_angle)
-
-	var rotation_amount = angular_speed * delta * direction
-
-	if velocity.length() > 0:
-		$GolemsPath.rotate(deg_to_rad(rotation_amount))
-
-	if abs(delta_angle) < 2.0:
-		angular_speed = 0
-	else:
-		angular_speed = 100.0
